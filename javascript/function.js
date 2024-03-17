1aec44e78eac7ab80dc019ef29fc0d75a81188dc
AustineBan <106940769+AustineBan@users.noreply.github.com>
2024-03-17 14:50:12 -0700

    Delete javascript directory

--- javascript/function.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.#menu');
-const nav = document.querySelector('nav');
-
-menu.addEventListener('click', () => {
-    console.log("was called");
-    nav.classList.remove('hide-element');
-})
